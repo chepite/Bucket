09f5da2a9ede0b7827bc5ff99081505a4ca60573
remyorwhtevr <remy.muylle@student.howest.be>
2021-11-30 09:30:33 +0100

    added style.css

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,12 +4,13 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style.css">
   <?php echo $css; ?>
-  <title>Bucket</title>
+  <title>Buck it!</title>
 </head>
 <body>
   <div class="container">
-      <header><h1 class="pagetitle"><a href="index.php">Bucket - Plan your future</a></h1></header>
+      <header><h1 class="pagetitle"><a href="index.php">Buck it</a></h1></header>
       <?php echo $content;?>
   </div>
   <?php echo $js; ?>
