68af12808fc5a059e266d564d3c4cb0fb043969f
remyorwhtevr <remy.muylle@student.howest.be>
2021-12-20 10:12:51 +0100

    styling submit button and centering search bar

--- src/view/user/index.php
+++ src/view/user/index.php
@@ -1,30 +1,32 @@
+<?php
+if(empty($_SESSION['userId'])){
+  echo('
+<div class="button__wrapper"><a class="button__login" href="index.php?page=login">login</a></div>' );
+}
+else{
+  echo('<p class="welcome__text">welcome back ' . $_SESSION['username'] . '</p>');
+  echo('<br><div class="button__wrapper"><a href="index.php?page=profile">Profile</a></div>
+');
+echo('<br><div class="button__wrapper"><a href="destroy.php"> Log Out</a></div>
+');
+}
+?>
 <!-- search form -->
 <form action="index.php" class="searchform">
     <div class="formLine">
     <label for="search"></label>
     <input class="search__bar" type="text" placeholder="Looking for new lists?" required name="searchtext" value="<?php
           if (!empty($_GET['searchtext'])) echo $_GET['searchtext'];?>">
-    <input class="submit__button" type="submit">
+    <input class="submit__button hidden" type="submit">
     </div>
 
 
 </form>
 <!-- end search form -->
-<?php
-if(empty($_SESSION['userId'])){
-  echo('
-<a class="button__login" href="index.php?page=login">login</a>' );
-}
-else{
-  echo('welcome back '.$_SESSION['username']);
-  echo('<br><a href="index.php?page=profile">Profile</a>
-');
-echo('<br><a href="destroy.php"> Log Out</a>
-');
-}
-?>
+
 <div class="bucketlists">
 </div>
 
 <script src="./js/search.js"></script>
+
 
