af9f68921b7bc9f053ce835f62cdd0613512630d
remyorwhtevr <remy.muylle@student.howest.be>
2021-12-21 16:20:25 +0100

    profile is fully done + responsive

--- src/js/profile.js
+++ src/js/profile.js
@@ -3,7 +3,15 @@
   const fillList=(data)=>{
     const $location = document.querySelector(".bucketlistList");
     $location.innerHTML = data.map((bucketlist)=>{
-      return `<li><a href="index.php?page=detail&id=${bucketlist.id}">${bucketlist.name}<a></li>`;
+      return `
+      <li>
+        <div class="bucketlist__wrapper">
+          <a href="index.php?page=detail&id=${bucketlist.id}">
+            <div class="bucketlist__name">${bucketlist.name}</div>
+            <div class="bucketlist__description"><p>Description:</p>${bucketlist.description}</div>
+          </a>
+        </div>
+      </li>`;
     }).join("");
   }
   const bucketlists = async ()=>{
