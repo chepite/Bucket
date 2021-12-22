bfa1b95f1b668b332cfc99d79dd3508b4beede46
chepite <julesvictor@telenet.be>
2021-12-22 02:24:49 +0100

    add activity appear and dissapear in new layout for #8

--- src/view/bucketlist/detail.php
+++ src/view/bucketlist/detail.php
@@ -42,13 +42,47 @@ echo('
 <div class="detail__wrapper">
   <div class="bucketlist__title">'. $bucketlist["name"] .'</div>
   <div class="editDiv"><a class="edit-link">Edit</a></div>
+
   <a class="threedot">...</a>
+
   <div class="description__wrapper">
     <p>'. $bucketlist["description"] .'</p>
   </div>
   <div class="activ__wrapper">
     <ul class="activityList">
     </ul>
+      <li>
+      <a class="addActivityLink" href="">Add new activity</a>
+      </li>');
+
+if(isset($_SESSION["userId"]) && $_SESSION['userId']=== $bucketlist["user_id"]){
+echo('
+<div class="addActivity hidden ">
+  <form id="addActivityForm" method="post" action="index.php?page=detail&id='. $_SESSION["detailBucketlist"].'">
+  <input type="hidden" name="action" value="addActivity">
+  <input type="text" name="name" required placeholder="Bucketlist name" size="32"></br>
+    <input type="date" name="date" required placeholder="Bucketlist date"></br>
+    <input type="text" name="place" required placeholder="Bucketlist place" size="255"></br>
+    <input type="number" name="price" required placeholder="Bucketlist price" ></br>
+    <input type="text" name="company" required placeholder="Bucketlist company" size="255"></br>
+    <input list="categorydatalist" id="categories" name="category" required/>
+    <datalist  id="categorydatalist" name="categorydatalist">
+            <select class="categorydatalist--list" name="categorieDataListSelect">');
+
+            foreach($categories as $category){
+             echo ('<option value="'.$category->id.'">'.$category->name.'</option>');
+            }
+          echo('
+            </select>
+          </datalist>
+  <input type="submit" value="add new activity">
+  </form>
+      </div>
+          ');}
+
+
+
+echo('
   </div>
 </div>
 
@@ -79,31 +113,31 @@ echo('
 ');}?>
 
 <?php
-if(isset($_SESSION["userId"]) && $_SESSION['userId']=== $bucketlist["user_id"]){
-echo('
-<a class="addActivityLink" href="">Add new activity</a>
-<div class="addActivity hidden ">
-  <form id="addActivityForm" method="post" action="index.php?page=detail&id='. $_SESSION["detailBucketlist"].'">
-  <input type="hidden" name="action" value="addActivity">
-  <input type="text" name="name" required placeholder="Bucketlist name" size="32"></br>
-    <input type="date" name="date" required placeholder="Bucketlist date"></br>
-    <input type="text" name="place" required placeholder="Bucketlist place" size="255"></br>
-    <input type="number" name="price" required placeholder="Bucketlist price" ></br>
-    <input type="text" name="company" required placeholder="Bucketlist company" size="255"></br>
-    <input list="categorydatalist" id="categories" name="category" required/>
-    <datalist  id="categorydatalist" name="categorydatalist">
-            <select class="categorydatalist--list" name="categorieDataListSelect">');
+// if(isset($_SESSION["userId"]) && $_SESSION['userId']=== $bucketlist["user_id"]){
+// echo('
+// <a class="addActivityLink" href="">Add new activity</a>
+// <div class="addActivity hidden ">
+//   <form id="addActivityForm" method="post" action="index.php?page=detail&id='. $_SESSION["detailBucketlist"].'">
+//   <input type="hidden" name="action" value="addActivity">
+//   <input type="text" name="name" required placeholder="Bucketlist name" size="32"></br>
+//     <input type="date" name="date" required placeholder="Bucketlist date"></br>
+//     <input type="text" name="place" required placeholder="Bucketlist place" size="255"></br>
+//     <input type="number" name="price" required placeholder="Bucketlist price" ></br>
+//     <input type="text" name="company" required placeholder="Bucketlist company" size="255"></br>
+//     <input list="categorydatalist" id="categories" name="category" required/>
+//     <datalist  id="categorydatalist" name="categorydatalist">
+//             <select class="categorydatalist--list" name="categorieDataListSelect">');
 
-            foreach($categories as $category){
-             echo ('<option value="'.$category->id.'">'.$category->name.'</option>');
-            }
-          echo('
-            </select>
-          </datalist>
-  <input type="submit" value="add new activity">
-  </form>
-      </div>
-          ');}?>
+//             foreach($categories as $category){
+//              echo ('<option value="'.$category->id.'">'.$category->name.'</option>');
+//             }
+//           echo('
+//             </select>
+//           </datalist>
+//   <input type="submit" value="add new activity">
+//   </form>
+//       </div>
+//           ');}?>
 
 <?php
 if(isset($_SESSION["userId"]) && $_SESSION["userId"] == $bucketlist["user_id"]){
